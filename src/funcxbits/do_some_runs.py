d4acdd5ead83370f62fa3e86c9d2f6fe1ace78a9
Ben Clifford <benc@hawaga.org.uk>
2022-02-22 19:45:52 +0000

    Target dev cluster endpoint; make test app return its own timings

--- src/funcxbits/do_some_runs.py
+++ src/funcxbits/do_some_runs.py
@@ -5,17 +5,26 @@ import logging
 
 logger = logging.getLogger("dnpc.funcx.demoapp")
 
+# production tutorial endpoint
+# target_endpoint = '4b116d3c-1703-4f8f-9f6f-39921e5864df'
 
-target_endpoint = '4b116d3c-1703-4f8f-9f6f-39921e5864df'
+funcx_service="https://api.dev.funcx.org/v2"
+target_endpoint="2238617a-8756-4030-a8ab-44ffb1446092"
+# other mess from test conftest.py that might be useful:
+#            "results_ws_uri": "wss://api.dev.funcx.org/ws/v2/",
+
 
 def sleep(duration=30):
     import time
+    start=time.time()
     time.sleep(duration)
+    end=time.time()
+    return (start, end)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG, filename="do_some_runs.log", format="%(created)f %(asctime)s %(name)s:%(lineno)d %(processName)s(%(process)d) %(threadName)s [%(levelname)s]  %(message)s")
     logger.info("APP CREATE_FUNCX_CLIENT")
-    c = funcx.FuncXClient()
+    c = funcx.FuncXClient(funcx_service_address=funcx_service)
 
     logger.info("APP REGISTER_FUNCTION")
     sleep_uuid = c.register_function(sleep)
@@ -23,7 +32,7 @@ if __name__ == "__main__":
 
     result_refs = []
     logger.info("APP LAUNCH_STAGE")
-    for n in range(0,500):
+    for n in range(0,20):
       logger.info(f"TASK {n} RUN")
       result_uuid = c.run(endpoint_id = target_endpoint, function_id=sleep_uuid)
       logger.info(f"TASK {n} RUN_POST {result_uuid}")
@@ -47,8 +56,9 @@ if __name__ == "__main__":
                 logger.info(f"TASK {n} POLL_END_PENDING_{e.reason}")
                 pass
             else:
-                print(r)
                 logger.info(f"TASK {n} POLL_END_COMPLETE")
+                logger.info(f"TASK_INNER_TIME {n} {r[0]} {r[1]}")
+                print(f"Task {n} complete")
         logger.info(f"POLL_LOOP_END {loop_count}")
 
     logger.info("APP END_MAIN")
